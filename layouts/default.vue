b7d9d1f3d2bbe9609942098ff8ae6c98f8e510a1
RemiSaurel <r.saurel5@gmail.com>
2024-10-19 08:08:22 +0200

    fix: icons size + color

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,28 +14,40 @@ const toProjects = () => {
 
 <template>
   <div class="max-w-6xl m-auto pb-4">
-    <nav class="flex items-center justify-end gap-3 p-2">
-      <button
-        @click="toHome"
-        class="size-8 rounded-lg border-0 hover:cursor-pointer flex items-center justify-center bg-white dark:bg-zinc-9 hover:bg-gray-100 dark:hover:bg-zinc-8"
-      >
-        <Icon name="lucide:house" class="size-5 dark:text-zinc-2" />
-      </button>
-      <button
-        @click="toPosts"
-        class="size-8 rounded-lg border-0 hover:cursor-pointer flex items-center justify-center bg-white dark:bg-zinc-9 hover:bg-gray-100 dark:hover:bg-zinc-8"
-      >
-        <Icon name="uil:book-alt" class="size-5 dark:text-zinc-2" />
-      </button>
-      <button
-        @click="toProjects"
-        class="size-8 rounded-lg border-0 hover:cursor-pointer flex items-center justify-center bg-white dark:bg-zinc-9 hover:bg-gray-100 dark:hover:bg-zinc-8"
-      >
-        <Icon name="lucide:laptop" class="size-5 dark:text-zinc-2" />
-      </button>
+    <ClientOnly>
+      <nav class="flex items-center justify-end gap-3 p-2">
+        <button
+          @click="toHome"
+          class="size-8 rounded-lg border-0 hover:cursor-pointer flex items-center justify-center bg-white dark:bg-zinc-9 hover:bg-gray-100 dark:hover:bg-zinc-8 p-0"
+        >
+          <Icon
+            name="lucide:house"
+            class="size-5 dark:text-zinc-2 text-zinc-9"
+          />
+        </button>
+        <button
+          @click="toPosts"
+          class="size-8 rounded-lg border-0 hover:cursor-pointer flex items-center justify-center bg-white dark:bg-zinc-9 hover:bg-gray-100 dark:hover:bg-zinc-8 p-0"
+        >
+          <Icon
+            name="uil:book-alt"
+            class="size-5 dark:text-zinc-2 text-zinc-9"
+          />
+        </button>
+        <button
+          @click="toProjects"
+          class="size-8 rounded-lg border-0 hover:cursor-pointer flex items-center justify-center bg-white dark:bg-zinc-9 hover:bg-gray-100 dark:hover:bg-zinc-8 p-0"
+        >
+          <Icon
+            name="lucide:laptop"
+            class="size-5 dark:text-zinc-2 text-zinc-9"
+          />
+        </button>
 
-      <ColorModeSwitcher />
-    </nav>
+        <ColorModeSwitcher />
+      </nav>
+    </ClientOnly>
+
     <div class="px-3 md:px-0 max-w-3xl m-auto mt-12">
       <slot />
     </div>
