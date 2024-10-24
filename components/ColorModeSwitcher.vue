08e3c5a85b536211f047ff88368fe9d8d4064874
barbapapazes <e.soubiran25@gmail.com>
2024-10-24 15:19:14 +0200

    fix: color mode switcher

--- components/ColorModeSwitcher.vue
+++ components/ColorModeSwitcher.vue
@@ -11,11 +11,13 @@ const toggleColorMode = () => {
     @click="toggleColorMode"
     class="size-8 rounded-lg border-0 hover:cursor-pointer flex items-center justify-center bg-white dark:bg-zinc-9 hover:bg-gray-100 dark:hover:bg-zinc-8 p-0"
   >
-    <Icon
-      name="uil:moon"
-      class="size-5 text-zinc-2"
-      v-if="colorMode.preference === 'dark'"
-    />
-    <Icon name="uil:sun" class="size-5" v-else />
+    <ClientOnly>
+      <Icon
+        name="uil:moon"
+        class="size-5 text-zinc-2"
+        v-if="colorMode.preference === 'dark'"
+      />
+      <Icon name="uil:sun" class="size-5" v-else />
+    </ClientOnly>
   </button>
 </template>
