214824f2354f40d3d47601726ba245bc2c065134
RemiSaurel <r.saurel5@gmail.com>
2024-10-29 11:46:12 +0100

    fix: useragent

--- server/api/github.ts
+++ server/api/github.ts
@@ -8,6 +8,7 @@ export default defineEventHandler(async (event) => {
     {
       headers: {
         Authorization: `Bearer ${config.githubToken}`,
+        UserAgent: "remisaurel",
       },
     }
   );
