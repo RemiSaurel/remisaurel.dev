db6ffdb5d6e9d9189935f8a4e5818848740f803f
RemiSaurel <r.saurel5@gmail.com>
2024-10-29 23:43:06 +0100

    fix: useragent

--- server/api/github.ts
+++ server/api/github.ts
@@ -2,7 +2,13 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig(event);
 
   const data = await $fetch(
-    "https://api.github.com/users/remisaurel/events?per_page=20"
+    "https://api.github.com/users/remisaurel/events?per_page=20",
+    {
+      headers: {
+        Authorization: `Bearer ${config.githubToken}`,
+        "User-Agent": "remisaurel",
+      },
+    }
   );
 
   return data;
