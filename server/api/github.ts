132754934735ac1fa22cc85b1a58300d09fc6758
RemiSaurel <r.saurel5@gmail.com>
2024-10-29 23:36:49 +0100

    fix: remove GH token

--- server/api/github.ts
+++ server/api/github.ts
@@ -2,13 +2,7 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig(event);
 
   const data = await $fetch(
-    "https://api.github.com/users/remisaurel/events?per_page=20",
-    {
-      headers: {
-        Authorization: `Bearer ${config.githubToken}`,
-        UserAgent: "request",
-      },
-    }
+    "https://api.github.com/users/remisaurel/events?per_page=20"
   );
 
   return data;
